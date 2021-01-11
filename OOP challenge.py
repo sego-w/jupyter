e9a0548c44c8d617dd4b4aee4e5bbe72e4e41daa
sego-w <segow090@gmail.com>
2021-01-11 21:54:06 +0200

    I honestly don't understand what I did to fix it... these terminal/shell differences are getting on my nerves

--- OOP challenge.py
+++ OOP challenge.py
@@ -1,15 +1,22 @@
 # Python Course OOP Challenge
 
 class Bank:
-    def __init__(self,ownerarg,balancearg):
+    def __init__(self,ownerarg,balancearg=0):
         self.owner = ownerarg
         self.balance = balancearg
     def deposit(self,deposit_amount):
-        self.deposit = deposit_amount
+       # self.deposit = deposit_amount
         self.balance = self.balance + deposit_amount
+        print(f"The new balance is {self.balance}")
     def withdrawal(self,withdraw_amount):
-        self.withdraw = withdraw_amount
-        self.balance = self.balance - withdraw_amount
+       # self.withdraw = withdraw_amount
+        if withdraw_amount <= self.balance:
+            self.balance = self.balance - withdraw_amount
+        else:
+            print("Error lolz")
+        print(f"The new balance is {self.balance}")
+    def __str__(self):
+        return f"The owner of the account is {exampl1.owner()}, with a balance of ${exampl1.balance()}"
 exampl1 = Bank("John Cena", 1000000)
-exampl1.deposit(13024)
-print(f"The owner of the account is {exampl1.owner()}, with a balance of ${exampl1.balance()}"
+exampl1.deposit(348734983)
+# print()
